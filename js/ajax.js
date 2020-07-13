14abe94f59c5cf3f76e32d97839e657bfd616da5
Paul <Paul@Admins-iMac.broadband>
2020-07-13 13:40:32 +0100

    доработан AJAX

---
+++ js/ajax.js
@@ -0,0 +1,33 @@
+'use strict';
+
+(function () {
+
+  window.ajax = function (type, data) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    if (type === 'GET') {
+      var URL = 'https://javascript.pages.academy/keksobooking/data';
+
+      xhr.addEventListener('load', function () {
+        window.updateMap(xhr.response, window.filter);
+      });
+    }
+
+    if (type === 'POST') {
+      URL = 'https://javascript.pages.academy/keksobooking';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.statusText === 'OK') {
+          window.onSuccess();
+        } else {
+          window.onError();
+        }
+      });
+    }
+
+    xhr.open(type, URL);
+    xhr.send(data);
+  };
+
+})();
