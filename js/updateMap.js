a5c7fa0c2648367a62cd2581e7c6e3a0b9402824
Paul <Paul@Admins-iMac.broadband>
2020-07-20 14:14:39 +0100

    подключены фильтры

--- js/updateMap.js
+++ js/updateMap.js
@@ -43,22 +43,155 @@
     }
   }
 
+  function checkPropertyType(offers, filter) {
+    var newOffersTemp = [];
+    if (filter.propertyType === 'any') {
+      newOffersTemp = offers;
+    } else {
+      for (var i = 0; i < offers.length; i++) {
+        if (offers[i].offer.type === filter.propertyType) {
+          newOffersTemp.push(offers[i]);
+        }
+      }
+    }
+    return newOffersTemp;
+  }
+
+  function checkPropertyPrice(offers, filter) {
+    if (filter.propertyPrice !== 'any') {
+      var newOffersTemp = [];
+
+      for (var i = 0; i < offers.length; i++) {
+        switch (filter.propertyPrice) {
+          case 'low':
+            if (offers[i].offer.price <= 10000) {
+              newOffersTemp.push(offers[i]);
+            }
+            break;
+          case 'high':
+            if (offers[i].offer.price >= 50000) {
+              newOffersTemp.push(offers[i]);
+            }
+            break;
+          case 'middle':
+            if (offers[i].offer.price > 10000 && offers[i].offer.price < 50000) {
+              newOffersTemp.push(offers[i]);
+            }
+            break;
+        }
+      }
+    } else {
+      newOffersTemp = offers;
+    }
+    return newOffersTemp;
+  }
+
+  function checkPropertyRooms(offers, filter) {
+    if (filter.propertyRooms !== 'any') {
+      var newOffersTemp = [];
+
+      for (var i = 0; i < offers.length; i++) {
+        switch (filter.propertyRooms) {
+          case '1':
+            if (String(offers[i].offer.rooms) === '1') {
+              newOffersTemp.push(offers[i]);
+            }
+            break;
+          case '2':
+            if (String(offers[i].offer.rooms) === '2') {
+              newOffersTemp.push(offers[i]);
+            }
+            break;
+          case '3':
+            if (String(offers[i].offer.rooms) === '3') {
+              newOffersTemp.push(offers[i]);
+            }
+            break;
+        }
+      }
+    } else {
+      newOffersTemp = offers;
+    }
+    return newOffersTemp;
+  }
+
+  function checkPropertyGuests(offers, filter) {
+    if (filter.propertyGuests !== 'any') {
+      var newOffersTemp = [];
+
+      for (var i = 0; i < offers.length; i++) {
+        switch (String(offers[i].offer.guests)) {
+          case '1':
+            if (filter.propertyGuests === '1') {
+              newOffersTemp.push(offers[i]);
+            }
+            break;
+          case '2':
+            if (filter.propertyGuests === '2') {
+              newOffersTemp.push(offers[i]);
+            }
+            break;
+          default:
+            if (filter.propertyGuests === '0') {
+              newOffersTemp.push(offers[i]);
+            }
+            break;
+        }
+      }
+    } else {
+      newOffersTemp = offers;
+    }
+    return newOffersTemp;
+  }
+
+  function checkFeatures(offers, type) {
+    var newOffersTemp = [];
+    for (var i = 0; i < offers.length; i++) {
+      for (var j = 0; j < offers[i].offer.features.length; j++) {
+        if (offers[i].offer.features[j] === type) {
+          newOffersTemp.push(offers[i]);
+        }
+      }
+    }
+    return newOffersTemp;
+  }
+
   window.updateMap = function (offers, filter) {
     removeAllOffers();
     removeAllCards();
+    var newOffers = [];
 
-    if (filter.propertyType === 'any') {
-      renderMap(offers);
-      window.card.renderCards(offers);
-    } else {
-      var newOffers = [];
-      for (var i = 0; i < offers.length; i++) {
-        if (offers[i].offer.type === filter.propertyType) {
-          newOffers.push(offers[i]);
-        }
-      }
-      renderMap(newOffers);
-      window.card.renderCards(newOffers);
+    newOffers = checkPropertyType(offers, filter);
+    newOffers = checkPropertyPrice(newOffers, filter);
+    newOffers = checkPropertyRooms(newOffers, filter);
+    newOffers = checkPropertyGuests(newOffers, filter);
+
+    if (filter.propertyFeatures.wifi === 'On') {
+      newOffers = checkFeatures(newOffers, 'wifi');
     }
+
+    if (filter.propertyFeatures.dishwasher === 'On') {
+      newOffers = checkFeatures(newOffers, 'dishwasher');
+    }
+
+    if (filter.propertyFeatures.parking === 'On') {
+      newOffers = checkFeatures(newOffers, 'parking');
+    }
+
+    if (filter.propertyFeatures.washer === 'On') {
+      newOffers = checkFeatures(newOffers, 'washer');
+    }
+
+    if (filter.propertyFeatures.elevator === 'On') {
+      newOffers = checkFeatures(newOffers, 'elevator');
+    }
+
+    if (filter.propertyFeatures.conditioner === 'On') {
+      newOffers = checkFeatures(newOffers, 'conditioner');
+    }
+
+    renderMap(newOffers);
+    window.card.renderCards(newOffers);
   };
+
 })();
